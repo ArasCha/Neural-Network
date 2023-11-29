d5e92f9ca34e809b7cab2f411c115207febc446c
Aras <araschaigne@gmail.com>
2023-11-29 20:27:37 +0100

    [ADD] Ability to take test datasets for analysis
    [EDIT] accuracy is not a property but a method
    [EDIT] train method takes a dataset in parameters and applies them to the model for analysis
    [EDIT] errors is not in the Perceptron oject anymore and is returned by train(Ã) which also returns accuracies

--- Perceptron.py
+++ Perceptron.py
@@ -15,18 +15,30 @@ class Perceptron:
         self.activation_function = activation_function
         self.cost_function = cost_function
         self.W, self.b = self.init_weights()
-        self.errors = []
 
-    def train(self, epochs: int, η: float = 0.05):
+    def train(self, epochs: int = 100, η: float = 0.05, X_test: np.ndarray = None, Y_test: np.ndarray = None):
 
+        errors = []
+        accuracies = []
+        errors_test = []
+        accuracies_test = []
+        
         for i in range(epochs):
             
-            A = self.model()
+            A = self.model(self.X)
 
             L = self.cost_function(A, self.Y)
-            self.errors.append(L.value())
+            errors.append(L.value())
+            accuracies.append(self.accuracy())
+            
+            if X_test is not None and Y_test is not None:
+                L_test = self.cost_function(self.model(X_test), Y_test)
+                errors_test.append(L_test.value())
+                accuracies_test.append(self.accuracy(X_test, Y_test))
 
             self.W, self.b = self.calculate_new_weights(η, L)
+
+        return errors, accuracies, errors_test, accuracies_test
 
     def init_weights(self) -> tuple[np.ndarray, float]:
         W = np.random.random_sample(size=self.X.shape[1])
@@ -34,18 +46,17 @@ class Perceptron:
         W = W.reshape(len(W), 1)
         return W, b
 
-    def calculate_Z(self, X: np.ndarray = None) -> np.ndarray:
+    def calculate_Z(self, X: np.ndarray) -> np.ndarray:
         """
         Returns X·W+b
         """
-        if X is None: X = self.X # in case we use it to get a prediction
 
         assert X.shape[1] == self.W.shape[0], "Given number of features must be equal to number of weights"
         arr = X.dot(self.W) + self.b
         arr = arr.reshape(len(arr), 1)
         return arr
     
-    def model(self, X: np.ndarray = None) -> np.ndarray:
+    def model(self, X: np.ndarray) -> np.ndarray:
         """
         Returns a vector of each element of a vector Z applied to the activation function f, aka the model
         X: Sample
@@ -73,14 +84,23 @@ class Perceptron:
 
     def predict(self, X: np.ndarray):
         """
-        X: Sample of one row to predict
+        X: Sample of one or many row to predict
+        Returns a bool if X is one sample or an ndarray of bools if many samples
         """
 
         A = self.model(X)
         return A >= 0.5 # should depend on the activation function used
     
-    @property
-    def accuracy(self) -> float:
+    def accuracy(self, X: np.ndarray = None, Y: np.ndarray = None) -> float:
+        """
+        Returns the accuracy of the model
+        """
+
         from sklearn.metrics import accuracy_score
-        Y_pred = self.predict(self.X)
-        return accuracy_score(self.Y, Y_pred)
+
+        if X is not None:
+            Y_pred = self.predict(X) # if we want to test the accuracy
+            return accuracy_score(Y, Y_pred)
+        else:
+            Y_pred = self.predict(self.X)
+            return accuracy_score(self.Y, Y_pred)
