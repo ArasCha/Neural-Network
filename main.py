4f53629a637d7195bb1001fcfee8dc12d9a4bedf
Aras <araschaigne@gmail.com>
2023-12-11 17:17:45 +0100

    [EDIT] main code simpler [ADD] with make_circles dataset

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Cost import LogLoss
 from ActivationFunction import sigmoid
 from utils import *
 from NeuralNetwork import NeuralNetwork
-
+from sklearn.datasets import make_circles
 
 
 
@@ -17,24 +17,19 @@ if __name__ == "__main__":
 
     X = normalize_images(X_images)
     X_test_normalized = normalize_images(X_test)
+    X_test_normalized = X_test_normalized.T
+    y_test = y_test.reshape((1, y_test.shape[0]))
 
     # perceptron = Perceptron(X, Y, sigmoid, LogLoss)
     # errors, accuracies, errors_test, accuracies_test = perceptron.train(10000, 0.01, X_test_normalized, y_test)
 
+    X, Y = make_circles(n_samples=100, noise=0.1, factor=0.3, random_state=0)
 
     X = X.T
     Y = Y.reshape((1, Y.shape[0]))
 
     nn = NeuralNetwork(X, Y, [8, 16, 32], sigmoid)
 
-    X_test_normalized = X_test_normalized.T
-    y_test = y_test.reshape((1, y_test.shape[0]))
-
-    errors, accuracies, errors_test, accuracies_test = nn.train(100, 0.1, X_test_normalized, y_test)
-    
-    # image, value = X_test[6], y_test[6]
-    # # show_images(image, value) # show the image we chose
-    # image_n = normalize_images(image)
-    # print(perceptron.predict(image_n)) # False: cat, True: dog
+    errors, accuracies, errors_test, accuracies_test = nn.train(100, 0.1)
     
     analysis(errors, errors_test, accuracies, accuracies_test)
